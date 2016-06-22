fd3b33017db4b156ab97249ecf27184b71e4782e
David Gibbs <david.gibbs@dressipi.com>
2016-06-22 21:46:46 +0100

    Initial commit

---
+++ CalculatorSwift/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  CalculatorSwift
+//
+//  Created by David Gibbs on 07/05/2016.
+//  Copyright © 2016 SixtySticks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyinDisplay = display.text!
+            display.text! = textCurrentlyinDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+}
+
